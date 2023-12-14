9eaf53ec847342f87acf40015782ea3089bd5876
Poltorak Serguei <ps@z-wave.me>
2023-12-14 17:22:17 +0300

    bootloader added and compiler improved

--- README.md
+++ README.md
@@ -9,6 +9,8 @@ rmdir -R sketch && mkdir -p sketch/tmp
 # copy your sketch to sketch/tmp/tmp.ino
 docker run -v $(pwd)/sketch:/sketch -it z-uno-compilation-server bash /build/compile.sh
 # output file is in sketch/tmp/tmp_ino_signed.bin
+# log in sketch/tmp/log.txt
+# compiler exit code in sketch/tmp/status.txt
 ```
 
 # Building the container
--- compile.sh
+++ compile.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+CHIP=$1
+NAME=$2
+
 ${BUILD_DIR}/zme_make/zme_make build /sketch/tmp/tmp.ino \
 	-S $(find ${BUILD_DIR} -name cores) \
 	-S $(find ${BUILD_DIR} -name libraries) \
@@ -8,5 +11,7 @@ ${BUILD_DIR}/zme_make/zme_make build /sketch/tmp/tmp.ino \
 	-lc $(find ${BUILD_DIR} -name libclang.so -exec dirname {} \;) \
 	-B /sketch/ \
 	-O make_listing \
-	-O BO:-DARDUINO=152 \
-	-O BO:-DARDUINO_ARCH_ZUNOG2
+	-O "BO:-DZUNO_SKETCH_NAME=\"${NAME}\"" \
+	-C ${CHIP} > /sketch/tmp/log.txt
+
+echo $? > /sketch/tmp/status.txt
